56cee271fff2ca3a1b016b69856549ca1aa3d346
Abhinav359810 <abhinavsinghairclay@gmail.com>
2026-07-15 03:47:03 +0530

    Configuring Express-Sessions and cors

--- Backend/app.js
+++ Backend/app.js
@@ -4,15 +4,31 @@
     const mongoose = require("mongoose");
     const cors = require("cors");
     const ExpressError = require("./utils/ExpressError.js");
+    const session = require("express-session");
 
     const listings = require("./routes/listing.js");
     const reviews = require("./routes/review.js");
 
     const MONGO_URL = "mongodb://127.0.0.1:27017/WanderStay";
 
+    const sessionOptions = {
+        secret : "74656%%^#*@(@)@jrfkeorfjkowedo",
+        resave : false,
+        saveUninitialized : true,
+        cookie: {
+            expires : Date.now() + 1000 * 60 * 60 * 24 * 3,
+            maxAge : 1000 * 60 * 60 * 24 * 3,
+            httpOnly : true
+        },
+    };
+
     //middlewares
-    app.use(cors());
+    app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    }));
     app.use(express.json())
+    app.use(session(sessionOptions));
     // app.use(express.urlencoded({extended:true})); for ejs
 
     //routes
