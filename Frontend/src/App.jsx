b4168fdbe8392bb801c2d6d7a59fb08bedfd1393
Abhinav359810 <abhinavsinghairclay@gmail.com>
2026-08-05 20:02:33 +0530

    fix: correct case-sensitive frontend imports

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { Routes,Route, Navigate } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/Index.jsx";
 import Show from "./pages/Show.jsx";
-import NewForm from "./pages/NewForm";
-import Editform from "./pages/Editform";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import NewForm from "./pages/NewForm.jsx";
+import Editform from "./pages/Editform.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import PageNotFound from "./components/PageNotFound.jsx";
 import toast, { Toaster } from "react-hot-toast";
 import Signup from "./pages/Signup.jsx";
-import Login from "./pages/login.jsx";
+import Login from "./pages/Login.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
